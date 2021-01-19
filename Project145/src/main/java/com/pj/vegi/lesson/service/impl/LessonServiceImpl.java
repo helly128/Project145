b1bc0972bc616f95203479b13a57742f56fa473b
suyeon <new01270@naver.com>
2021-01-19 22:44:05 +0900

    으

--- Project145/src/main/java/com/pj/vegi/lesson/service/impl/LessonServiceImpl.java
+++ Project145/src/main/java/com/pj/vegi/lesson/service/impl/LessonServiceImpl.java
@@ -1,6 +1,9 @@
 package com.pj.vegi.lesson.service.impl;
 
+import org.springframework.stereotype.Service;
+
 import com.pj.vegi.lesson.service.LessonService;
 
+@Service("lessonService")
 public class LessonServiceImpl implements LessonService {
 }
