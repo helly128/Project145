217f8cadae9b87b0d0a9fffdc8f65225e29e8359
helly128 <jsjshr@naver.com>
2021-01-21 21:33:30 +0900

    merge 충돌 수정

--- Project145/src/main/java/com/pj/vegi/member/service/MemberService.java
+++ Project145/src/main/java/com/pj/vegi/member/service/MemberService.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.pj.vegi.member.service;
 
 import java.sql.SQLException;
@@ -15,20 +14,3 @@ public interface MemberService {
 	
 
 }
-=======
-package com.pj.vegi.member.service;
-
-import java.sql.SQLException;
-import com.pj.vegi.vo.MemberVo;
-
-public interface MemberService {
-
-//	public MemberVO getEmp(memberVO memberVO);
-//	public List<MemberVO> getMemberList(MemberVO memberVO);
-//	public void memberInsert(MemberVO memberVO);
-//	MemberVo memberSelect(MemberVo vo) throws SQLException;
-	boolean memberLoginCheck(MemberVo vo) throws SQLException;	// 멤버 로그인 체크
-	int memberInsert(MemberVo vo) throws SQLException;
-
-}
->>>>>>> branch 'main' of https://github.com/helly128/Project145.git
