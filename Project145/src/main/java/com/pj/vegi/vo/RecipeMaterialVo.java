562b7023988ba2e30149eb825fd2ba432469fee8
dhfl <dhfl1207@gmail.com>
2021-01-22 18:30:19 +0900

    크롤링 수정

--- Project145/src/main/java/com/pj/vegi/vo/RecipeMaterialVo.java
+++ Project145/src/main/java/com/pj/vegi/vo/RecipeMaterialVo.java
@@ -1,6 +1,5 @@
 package com.pj.vegi.vo;
 
-<<<<<<< HEAD
 import java.util.Date;
 
 import lombok.AllArgsConstructor;
@@ -13,19 +12,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RecipeMaterialVo {
-=======
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class RecipeMaterialVo {
-
->>>>>>> branch 'main' of https://github.com/helly128/Project145.git
 	private String matId;
 	private int matRecommend;
 	private String matName;
