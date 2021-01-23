a89d67eefa67ff291143f605051aa3a3f8b0e6ba
suyeon <new01270@naver.com>
2021-01-23 15:59:40 +0900

    .

--- Project145/src/main/java/com/pj/vegi/member/web/MemberController.java
+++ Project145/src/main/java/com/pj/vegi/member/web/MemberController.java
@@ -12,7 +12,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
