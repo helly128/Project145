6efe4ae4d7c2aed3455a43dfc963fad8b494ba74
admin <admin@YD02-23>
2021-01-25 12:39:01 +0900

    레스토랑

--- Project145/src/main/java/com/pj/vegi/restaurant/service/RestaurantService.java
+++ Project145/src/main/java/com/pj/vegi/restaurant/service/RestaurantService.java
@@ -1,6 +1,8 @@
 package com.pj.vegi.restaurant.service;
 
 import java.sql.SQLException;
+import java.util.List;
+
 import com.pj.vegi.vo.RestaurantVo;
 
 public interface RestaurantService {
@@ -8,6 +10,7 @@ public interface RestaurantService {
 
 	boolean restaurantCheck(RestaurantVo vo) throws SQLException;	// 멤버 로그인 체크
 	int restaurantInsert(RestaurantVo vo) throws SQLException;
+	List<RestaurantVo> bizCheckList(String restName) throws SQLException;
 	
 
 }
