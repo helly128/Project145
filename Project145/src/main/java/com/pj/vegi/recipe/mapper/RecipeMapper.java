d57ec02b08cb24c89b121fa7f0521b0cdc6b5fd1
chacha1301 <dosahjy@gmail.com>
2021-01-28 18:59:42 +0900

    네이버 검색 완료~~~

--- Project145/src/main/java/com/pj/vegi/recipe/mapper/RecipeMapper.java
+++ Project145/src/main/java/com/pj/vegi/recipe/mapper/RecipeMapper.java
@@ -6,10 +6,11 @@ import com.pj.vegi.vo.RecipeVo;
 
 public interface RecipeMapper {
 
-	List<RecipeVo>getRecipeList(RecipeVo vo);//레시피 전체 조회
-	
-	  RecipeVo recipeSelect(RecipeVo vo);//한 건 조회 
-	 // int recipeInsert(RecipeVo  vo);//추가 
-	  //int recipeUpdate(RecipeVo vo);//수정 
-	  //int recipeDelete(RecipeVo vo);//삭제
-	}
+	List<RecipeVo> getRecipeList(RecipeVo vo);// 레시피 전체 조회
+
+	RecipeVo recipeSelect(RecipeVo vo);// 한 건 조회
+	// int recipeInsert(RecipeVo vo);//추가
+	// int recipeUpdate(RecipeVo vo);//수정
+
+	int recipeDelete(RecipeVo vo);// 삭제
+}
--- Project145/src/main/java/com/pj/vegi/search/service/NaverSearchService.java
+++ Project145/src/main/java/com/pj/vegi/search/service/NaverSearchService.java
@@ -1,5 +1,11 @@
 package com.pj.vegi.search.service;
 
+import java.util.List;
+
+import com.pj.vegi.vo.RecipeMaterialVo;
+
 public interface NaverSearchService {
 
+	public List<RecipeMaterialVo> getMaterialList(RecipeMaterialVo vo);
+//	public RecipeVo recipeMaterialSelect(RecipeVo vo);
 }
