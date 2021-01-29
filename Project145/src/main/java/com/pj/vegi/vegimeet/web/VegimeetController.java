52b57c59e5f0f9ef121a2a5d92a3daabe0157ffc
helly128 <jsjshr@naver.com>
2021-01-29 18:14:11 +0900

    좋아요 수정, 베지밋 상세/결제 페이지 작업 중

--- Project145/src/main/java/com/pj/vegi/vegimeet/web/VegimeetController.java
+++ Project145/src/main/java/com/pj/vegi/vegimeet/web/VegimeetController.java
@@ -1,5 +1,6 @@
 package com.pj.vegi.vegimeet.web;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
@@ -11,14 +12,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.pj.vegi.member.service.MemberService;
 import com.pj.vegi.vegimeet.service.VegimeetService;
 import com.pj.vegi.vo.LikeListVo;
+import com.pj.vegi.vo.MemberVo;
 import com.pj.vegi.vo.VegimeetVo;
 
 @Controller
 public class VegimeetController {
 	@Autowired
 	VegimeetService vegimeetService;
+	@Autowired
+	MemberService memberService;
 	
 	//메인페이지 출력
 	@RequestMapping("/vegimeetList.do")
@@ -42,10 +47,14 @@ public class VegimeetController {
 	//상세페이지 출력
 	@RequestMapping("/vegimeetSelect.do")
 	public String vegimeetSelect(Model model, VegimeetVo vo) {
+		vo = vegimeetService.vegimeetSelect(vo);
 		
-		return "";
+		
+		model.addAttribute("meetVo", vo);
+		return "vegimeet/vegimeetSelect";
 	}
 	
+	//좋아요
 	@ResponseBody
 	@RequestMapping("/vegimeetLike.do/{meetId}")
 	public void vegimeetLike(@PathVariable String meetId, HttpSession session) {
@@ -55,6 +64,7 @@ public class VegimeetController {
 		vegimeetService.likeInsert(vo);
 	}
 	
+	//좋아요 취소
 	@ResponseBody
 	@RequestMapping("/vegimeetUnlike.do/{meetId}")
 	public void vegimeetUnlike(@PathVariable String meetId, HttpSession session) {
@@ -62,5 +72,24 @@ public class VegimeetController {
 		vo.setMId((String) session.getAttribute("mId"));
 		vo.setOriginId(meetId);
 		vegimeetService.likeDelete(vo);
+	}
+	
+	
+	@RequestMapping("/vegimeetJoinPage.do")
+	public String vegimeetJoinPage(Model model, VegimeetVo meetVo, HttpSession session) {
+		meetVo = vegimeetService.vegimeetSelect(meetVo);
+		
+		MemberVo memVo = new MemberVo();
+		memVo.setMId((String) session.getAttribute("mId"));
+		try {
+			memVo = memberService.memberSelect(memVo);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		model.addAttribute("meetVo", meetVo);
+		model.addAttribute("memberVo", memVo);
+		
+		return "vegimeet/payPage";
 	}
 }
