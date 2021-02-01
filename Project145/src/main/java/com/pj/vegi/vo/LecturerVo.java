fd12744917696b1b0b916db5df6342eb0ea5dc83
helly128 <jsjshr@naver.com>
2021-02-01 22:17:35 +0900

    lecturerVo 추가

---
+++ Project145/src/main/java/com/pj/vegi/vo/LecturerVo.java
@@ -0,0 +1,16 @@
+package com.pj.vegi.vo;
+
+import lombok.Data;
+
+@Data
+public class LecturerVo {
+
+	private String lecId;
+	private String mId;
+	private String cId;    
+	private String bizNum;      
+	private String lecCareer; 
+	private String proposal;       
+	private String response;  
+	
+}
