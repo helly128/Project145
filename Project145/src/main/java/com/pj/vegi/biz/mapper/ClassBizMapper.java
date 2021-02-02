363e174b64a3cb9f4a725efa727cef0107cd87ad
rachelistic <raychrayche@218.38.137.28>
2021-02-03 00:27:23 +0900

    클래스 사업자 CRU(Delete 대신 비활성화?)

--- Project145/src/main/java/com/pj/vegi/biz/mapper/ClassBizMapper.java
+++ Project145/src/main/java/com/pj/vegi/biz/mapper/ClassBizMapper.java
@@ -2,13 +2,20 @@ package com.pj.vegi.biz.mapper;
 
 import java.util.List;
 
+import com.pj.vegi.vo.LecturerVo;
 import com.pj.vegi.vo.LessonVO;
+import com.pj.vegi.vo.MemberVo;
 
 
 
 public interface ClassBizMapper {
 	List<LessonVO> classBizList(LessonVO vo);
 	LessonVO classBizSelect(LessonVO vo);
+	MemberVo classBizNum(MemberVo mvo);
+	int classBizInsert(LessonVO cvo);
+	int classLecturerInsert(LecturerVo lvo);
+	LecturerVo lecBizSelect(LecturerVo lvo);
+	
 
 
 	
--- Project145/src/main/java/com/pj/vegi/biz/service/impl/ClassBizServiceImpl.java
+++ Project145/src/main/java/com/pj/vegi/biz/service/impl/ClassBizServiceImpl.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Service;
 
 import com.pj.vegi.biz.mapper.ClassBizMapper;
 import com.pj.vegi.biz.service.ClassBizService;
+import com.pj.vegi.vo.LecturerVo;
 import com.pj.vegi.vo.LessonVO;
+import com.pj.vegi.vo.MemberVo;
 
 @Service("classBizService")
 public class ClassBizServiceImpl implements ClassBizService{
@@ -19,6 +21,36 @@ public class ClassBizServiceImpl implements ClassBizService{
 	public List<LessonVO> classBizList(LessonVO vo) {
 		
 		return dao.classBizList(vo);
+	}
+
+	@Override
+	public MemberVo classBizNum(MemberVo mvo) {
+		// TODO Auto-generated method stub
+		return dao.classBizNum(mvo);
+	}
+
+	@Override
+	public int classBizInsert(LessonVO cvo) {
+		int n = dao.classBizInsert(cvo);
+		return n;
+	}
+
+	@Override
+	public int classLecturerInsert(LecturerVo lvo) {
+		int n =dao.classLecturerInsert(lvo);
+		return n;
+	}
+
+	@Override
+	public LessonVO classBizSelect(LessonVO cvo) {
+		// TODO Auto-generated method stub
+		return dao.classBizSelect(cvo);
+	}
+
+	@Override
+	public LecturerVo lecBizSelect(LecturerVo lvo) {
+		// TODO Auto-generated method stub
+		return dao.lecBizSelect(lvo);
 	}
 
 }
